56f1165d45ba5f1a59e8a74b46b630c038ab0aeb
PeterPwlk <piotr.pawlak101@gmail.com>
2021-01-19 00:15:37 +0100

    global default cache

--- Backend/web-api/src/app.module.ts
+++ Backend/web-api/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { CacheInterceptor, CacheModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -9,6 +9,7 @@ import { TutorModule } from './Services/tutor/tutor.module';
 import { CourseModule } from './Services/course/course.module';
 import { FacultyModule } from './Services/faculty/faculty.module';
 import { LectureModule } from './Services/lecture/lecture.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -22,8 +23,18 @@ import { LectureModule } from './Services/lecture/lecture.module';
     CourseModule,
     FacultyModule,
     LectureModule,
+    CacheModule.register({
+      ttl: 300,
+      max: 5,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
