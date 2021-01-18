31798d57da2c6b5ee320ad2281135bc88dac3075
PeterPwlk <piotr.pawlak101@gmail.com>
2021-01-18 22:53:52 +0100

    added compression package

--- Backend/web-api/src/main.ts
+++ Backend/web-api/src/main.ts
@@ -3,10 +3,12 @@ import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { ConfigService } from '@nestjs/config';
 import { config } from 'aws-sdk';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  app.use(compression());
   app.setGlobalPrefix('api');
 
   const configService = app.get(ConfigService);
