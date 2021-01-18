ec505abc5fe1a2022635127c0550eff81cda4de9
Alicja Bielska <alicja.bielska97@gmail.com>
2021-01-18 15:54:45 +0100

    Lecture details

--- Backend/web-api/src/Services/lecture-schedule/lecture-schedule.service.ts
+++ Backend/web-api/src/Services/lecture-schedule/lecture-schedule.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { LectureSchedule } from '../../../../Persistance/Models/lectureSchedule';
 import { LectureScheduleRepositoryService } from './lecture-schedule-repository.service';
 import { FacultyRepositoryService } from '../faculty/faculty-repository.service';
+import { LectureRepositoryService } from '../lecture/lecture-repository.service';
 
 @Injectable()
 export class LectureScheduleService {
   constructor(
     private lectureScheduleRepository: LectureScheduleRepositoryService,
     private facultyRepository: FacultyRepositoryService,
+    private lectureRepository: LectureRepositoryService,
   ) {}
 
   async getAll(): Promise<LectureSchedule[]> {
@@ -22,8 +24,14 @@ export class LectureScheduleService {
   }
 
   async getById(id: number) {
-    const response = await this.lectureScheduleRepository.getById(id);
-    return response[0];
+    const lecturesWithDetails = [];
+    const lectureSchedule = await this.lectureScheduleRepository.getById(id);
+    for (const lectureId of lectureSchedule[0].lectures) {
+      const lectureDetails = await this.lectureRepository.getById(lectureId);
+      lecturesWithDetails.push(lectureDetails);
+    }
+    lectureSchedule[0].lectures = lecturesWithDetails;
+    return lectureSchedule;
   }
 
   async getByFacultyId(facultyId: number) {
