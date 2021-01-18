1c255423865a83fbff2dcc6bf540b6b76b896766
Konrad Kazieczko <kondziu989@gmail.com>
2021-01-18 14:21:40 +0100

    Improvements

---
+++ frontend/src/components/GroupDetailsCard.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "GroupDetailsCard"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/ManageGroup.vue
@@ -0,0 +1,13 @@
+<template>
+    $END$
+</template>
+
+<script>
+    export default {
+        name: "ManageGroup"
+    }
+</script>
+
+<style scoped>
+
+</style>
