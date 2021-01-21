42cf98789a4692f0ffa09f9abd3ac5b1e89f7dca
PeterPwlk <piotr.pawlak101@gmail.com>
2021-01-21 21:26:16 +0100

    added error handling to file reader

--- Backend/Persistance/Utils/jsonFileReader.ts
+++ Backend/Persistance/Utils/jsonFileReader.ts
@@ -3,8 +3,11 @@ import { readFileSync } from 'fs';
 export class JsonFileReader {
     readAndMap(path: string) {
         const file = readFileSync(path);
-        const data = JSON.parse(file);
-        console.log(`Read data from ${path} - count:${data.length}`);
-        return data;
+        if (typeof file === "string") {
+            const data = JSON.parse(file);
+            console.log(`Read data from ${path} - count:${data.length}`);
+            return data;
+        }
+        throw new Error(`Error reading file: ${path}`)
     }
 }
