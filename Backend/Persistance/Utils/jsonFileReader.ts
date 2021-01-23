8c2b855e29d684bc1566c497b3ea840b80da0f48
Alicja Bielska <alicja.bielska97@gmail.com>
2021-01-23 13:03:29 +0100

    Postman test

--- Backend/Persistance/Utils/jsonFileReader.ts
+++ Backend/Persistance/Utils/jsonFileReader.ts
@@ -3,8 +3,8 @@ import { readFileSync } from 'fs';
 export class JsonFileReader {
     readAndMap(path: string) {
         const file = readFileSync(path);
-        if (typeof file === "string") {
-            const data = JSON.parse(file);
+        if (typeof file === "object") {
+            const data = JSON.parse(file.toString());
             console.log(`Read data from ${path} - count:${data.length}`);
             return data;
         }
