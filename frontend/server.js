2013e3593ae94047a5b80207bba6a3d38ea0c6f5
Konrad Kazieczko <kondziu989@gmail.com>
2021-01-25 18:31:03 +0100

    Dockerfile improvement

---
+++ frontend/server.js
@@ -0,0 +1,10 @@
+const PORT = 8080;
+const express = require('express');
+const history = require('connect-history-api-fallback');
+const app = express();
+app.use(history());
+app.use(express.static('dist'));
+app.get('/', (req, res) => {
+  res.sendFile('dist/index.html');
+});
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
--- frontend/src/httpService/api.js
+++ frontend/src/httpService/api.js
@@ -2,10 +2,10 @@ import axios from 'axios'
 // import Cookies from 'js-cookie'
 
 export default axios.create({
-  baseURL: '/api',
+  // baseURL: process.env.VUE_APP_API_URL,
+  baseURL: 'http://localhost:3000/api',
   timeout: 20000,
   headers: {
     'Content-Type': 'application/json',
   }
 });
-
