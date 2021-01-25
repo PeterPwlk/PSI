bc2818208b17b06f80cfe9298b3b70e0c64cb22d
PeterPwlk <piotr.pawlak101@gmail.com>
2021-01-25 19:36:25 +0100

    added unit and interation tests for class-room package

--- Backend/web-api/src/Services/class-room/class-room.service.spec.ts
+++ Backend/web-api/src/Services/class-room/class-room.service.spec.ts
@@ -1,18 +1,84 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ClassRoomService } from './class-room.service';
+import { ClassRoomRepositoryService } from './class-room-repository.service';
+import { ClassRoom } from '../../../../Persistance/Models/classRoom';
+import { ClassRoomType } from '../../../../Persistance/Models/classRoomType';
 
 describe('ClassRoomService', () => {
   let service: ClassRoomService;
+  let repository: ClassRoomRepositoryService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ClassRoomService],
+      providers: [ClassRoomService, ClassRoomRepositoryService],
     }).compile();
 
     service = module.get<ClassRoomService>(ClassRoomService);
+    repository = module.get<ClassRoomRepositoryService>(
+      ClassRoomRepositoryService,
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should return class-room by id', async () => {
+    const expected: ClassRoom = {
+      number: '134',
+      building: 'A1',
+      capacity: 300,
+      classRoomId: 1,
+      classRoomType: ClassRoomType.Lecture,
+    };
+
+    const spyInstance = jest.spyOn(repository, 'getById');
+    spyInstance.mockImplementation(() => Promise.resolve(expected));
+
+    expect(await service.getById(1)).toBe(expected);
+    expect(spyInstance).toBeCalled();
+  });
+
+  it('should return all class-rooms', async () => {
+    const expected: ClassRoom[] = [
+      {
+        number: '134',
+        building: 'A1',
+        capacity: 300,
+        classRoomId: 1,
+        classRoomType: ClassRoomType.Lecture,
+      },
+      {
+        number: '134',
+        building: 'A1',
+        capacity: 300,
+        classRoomId: 1,
+        classRoomType: ClassRoomType.Lecture,
+      },
+    ];
+
+    const spyInstance = jest.spyOn(repository, 'getAll');
+    spyInstance.mockImplementation(() => Promise.resolve(expected));
+
+    expect(await service.getAll()).toBe(expected);
+    expect(spyInstance).toBeCalled();
+  });
+
+  it('should return class-room by id', async () => {
+    const expected: ClassRoom[] = [
+      {
+        number: '134',
+        building: 'A1',
+        capacity: 300,
+        classRoomId: 1,
+        classRoomType: ClassRoomType.Lecture,
+      },
+    ];
+
+    const spyInstance = jest.spyOn(repository, 'getByClassRoomType');
+    spyInstance.mockImplementation(() => Promise.resolve(expected));
+
+    expect(await service.getByClassRoomType(1)).toBe(expected);
+    expect(spyInstance).toBeCalled();
   });
 });
---
+++ Backend/web-api/test/class-room.e2e-spec.ts
@@ -0,0 +1,79 @@
+import * as request from 'supertest';
+import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+import { ClassRoomType } from '../../Persistance/Models/classRoomType';
+import { ClassRoomRepositoryService } from '../src/Services/class-room/class-room-repository.service';
+import { ClassRoomModule } from '../src/Services/class-room/class-room.module';
+
+describe('ClassRoomController', () => {
+  let app: INestApplication;
+  const classRoomRepository = {
+    getAll: () => [
+      {
+        number: '134',
+        building: 'A1',
+        capacity: 300,
+        classRoomId: 1,
+        classRoomType: ClassRoomType.Lecture,
+      },
+      {
+        number: '134',
+        building: 'A1',
+        capacity: 300,
+        classRoomId: 1,
+        classRoomType: ClassRoomType.Lecture,
+      },
+    ],
+    getById: () => ({
+      number: '134',
+      building: 'A1',
+      capacity: 300,
+      classRoomId: 1,
+      classRoomType: ClassRoomType.Lecture,
+    }),
+    getByClassRoomType: () => ({
+      number: '134',
+      building: 'A1',
+      capacity: 300,
+      classRoomId: 1,
+      classRoomType: ClassRoomType.Lecture,
+    }),
+  };
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [ClassRoomModule],
+    })
+      .overrideProvider(ClassRoomRepositoryService)
+      .useValue(classRoomRepository)
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('/GET class-room', () => {
+    return request(app.getHttpServer())
+      .get('/class-room')
+      .expect(200)
+      .expect(classRoomRepository.getAll());
+  });
+
+  it('/GET class-room', () => {
+    return request(app.getHttpServer())
+      .get('/class-room/1')
+      .expect(200)
+      .expect(classRoomRepository.getById());
+  });
+
+  it('/GET class-room', () => {
+    return request(app.getHttpServer())
+      .get('/class-room/filter?classRoomType=0')
+      .expect(200)
+      .expect(classRoomRepository.getById());
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
