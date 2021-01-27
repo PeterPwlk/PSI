4d15337bd712be353af8ddcbf8846c4c9dbea1b2
Konrad Kazieczko <kondziu989@gmail.com>
2021-01-27 21:13:08 +0100

    JWT cookie working finally!

--- frontend/server.js
+++ frontend/server.js
@@ -1,10 +1,19 @@
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 const express = require('express');
 const history = require('connect-history-api-fallback');
 const app = express();
+const proxy = require('express-http-proxy');
+
+app.use('/api', proxy(process.env.BACKEND_URL,{
+  proxyReqPathResolver: function(req) {
+    return `/api` + req.url
+  }
+}));
+
 app.use(history());
 app.use(express.static('dist'));
 app.get('/', (req, res) => {
   res.sendFile('dist/index.html');
 });
+
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
