58fd947c147687fcfaa96c72cccaeea4aa51b770
AlicjaBielska <alicja.bielska97@gmail.com>
2021-01-28 14:54:22 +0100

    express quick test

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,27 @@
-const PORT = process.env.PORT || 8080;
+const { readFileSync } = require('fs');
 const express = require('express');
+const http = require('http');
+const https = require('https');
 const history = require('connect-history-api-fallback');
-const app = express();
 const proxy = require('express-http-proxy');
 
-app.use('/api', proxy(process.env.BACKEND_URL,{
-  proxyReqPathResolver: function(req) {
-    return `/api` + req.url
-  }
-}));
+const PORT = process.env.PORT || 8080;
+
+const certs = {
+  key: readFileSync(process.env.SSL_KEY, 'utf-8'),
+  cert: readFileSync(process.env.SSL_CERT, 'utf-8'),
+};
+
+const app = express();
+
+app.use(
+  '/api',
+  proxy(process.env.BACKEND_URL, {
+    proxyReqPathResolver: function(req) {
+      return `/api` + req.url;
+    },
+  })
+);
 
 app.use(history());
 app.use(express.static('dist'));
@@ -16,4 +29,8 @@ app.get('/', (req, res) => {
   res.sendFile('dist/index.html');
 });
 
-app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
+const server = process.env.SSL
+  ? https.createServer(certs, app)
+  : http.createServer(app);
+
+server.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
