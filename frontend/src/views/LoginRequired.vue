3eaa10f86e7c88b087fd2e488853f095ccbf6b87
AlicjaBielska <alicja.bielska97@gmail.com>
2021-01-28 15:05:43 +0100

    unauth

--- frontend/src/views/LoginRequired.vue
+++ frontend/src/views/LoginRequired.vue
@@ -3,7 +3,7 @@
         <b-row class="h-100">
             <b-col align-self="center">
                 <h3> Aby korzystać z systemu logowania, musisz być zaplanowany! </h3>
-                <b-btn href="https://pwr-psi.auth.us-east-1.amazoncognito.com/login?client_id=4qkatbqtpu4bsv25b27cffvtjv&response_type=code&scope=openid+profile&redirect_uri=http://localhost:8080/login"> Zaloguj się </b-btn>
+                <b-btn href="https://nowa-edukacja.auth.us-east-1.amazoncognito.com/login?client_id=3gfjgnf29ngqvsp52pqmc1dn7f&response_type=code&scope=openid+profile&redirect_uri=https://51.83.129.128:8080/login"> Zaloguj się </b-btn>
             </b-col>
         </b-row>
     </b-container>
