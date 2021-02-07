08ecf37c5fac30985750f6840fa4bae9d8edadf8
AlicjaBielska <alicja.bielska97@gmail.com>
2021-02-07 16:28:58 +0100

    Guard auth added

--- Backend/web-api/src/Controllers/lecture-schedule/lecture-schedule.controller.ts
+++ Backend/web-api/src/Controllers/lecture-schedule/lecture-schedule.controller.ts
@@ -1,23 +1,27 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {Controller, Get, Param, Query, UseGuards} from '@nestjs/common';
 import { LectureScheduleService } from '../../Services/lecture-schedule/lecture-schedule.service';
 import { LectureSchedule } from '../../../../Persistance/Models/lectureSchedule';
 import {mapToLectureScheduleDTO} from "../../DTO/lectureScheduleDTO";
+import {JwtAuthGuard} from "../../auth/jwt-auth.guard";
 
 @Controller('lecture-schedule')
 export class LectureScheduleController {
   constructor(private lectureService: LectureScheduleService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   async getAll(): Promise<LectureSchedule[]> {
     return await this.lectureService.getAll();
   }
 
   @Get('filter')
+  @UseGuards(JwtAuthGuard)
   public async getByCourseId(@Query('facultyId') facultyId) {
     return await this.lectureService.getByFacultyId(parseInt(facultyId));
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   public async getById(@Param('id') id) {
     const response = await this.lectureService.getById(parseInt(id));
     return mapToLectureScheduleDTO([response])[0];
--- Backend/web-api/src/Controllers/tutor/tutor.controller.ts
+++ Backend/web-api/src/Controllers/tutor/tutor.controller.ts
@@ -1,16 +1,19 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import {Controller, Get, Param, Query, UseGuards} from '@nestjs/common';
 import { TutorService } from '../../Services/tutor/tutor.service';
+import {JwtAuthGuard} from "../../auth/jwt-auth.guard";
 
 @Controller('tutor')
 export class TutorController {
   constructor(private readonly tutorService: TutorService) {}
 
   @Get()
+  @UseGuards(JwtAuthGuard)
   public async getAll() {
     return await this.tutorService.getAll();
   }
 
   @Get('filter')
+  @UseGuards(JwtAuthGuard)
   public async getByCourseId(@Query('courseId') courseId) {
     return await this.tutorService.getAllBySuggestedCourseId(
       parseInt(courseId),
@@ -18,6 +21,7 @@ export class TutorController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   public async getById(@Param('id') id) {
     return await this.tutorService.getById(parseInt(id));
   }
